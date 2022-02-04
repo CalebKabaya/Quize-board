05d75f157be4ed844db416b7a90fd8a9933ecc49
calebkabaya <calebkabaya@gmail.com>
2022-02-04 17:44:52 +0300

    st:js functions- added a function to call answers

---
+++ main.js
@@ -0,0 +1,8 @@
+function mySubbmitButton(){
+    // document.write("hey there");
+ var correct = 0;
+ var currentAnswer = document.Questions.question1.value;
+ if (currentAnswer ==22){correct++};
+ document.write(correct);
+
+}
