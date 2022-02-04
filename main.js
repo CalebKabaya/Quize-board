ce8e5b283a467adf3870769838d3616ca96961c3
calebkabaya <calebkabaya@gmail.com>
2022-02-04 18:33:17 +0300

    ft:ft-changing -changed the questions

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function mySubbmitButton(){
     // document.write("hey there");
  var score = 0;
  var currentAnswer = document.Questions.question1.value;
+ var currentAnswer = document.Questions.question2.value;
+ var currentAnswer = document.Questions.question3.value;
+ var currentAnswer = document.Questions.question4.value;
+ var currentAnswer = document.Questions.question5.value;
+
  if (currentAnswer ==22){score+=10};
  document.write(score);
 
