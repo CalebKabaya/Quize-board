8d7079594e9e3b19e4bd55de286ff2f357d0efa2
calebkabaya <calebkabaya@gmail.com>
2022-02-05 05:06:41 +0300

    st:st-function,- addedd a function to display answer in new page

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ function mySubbmitButton(){
  var currentAnswer3 = document.Questions.question3.value;
  var currentAnswer4 = document.Questions.question4.value;
  var currentAnswer5= document.Questions.question5.value;
+ var results=document.getElementById("results");
 
  if (currentAnswer1 =="Hyper Text Markup Language"){score+=20};
  if (currentAnswer2 =="h1"){score+=20};
  if (currentAnswer3 =="alt"){score+=20};
  if (currentAnswer4 =="True"){score+=20};
  if (currentAnswer5 =="The World Web Consortium"){score+=20};
+ Questions.style.display="none";
+ results.textContent= score;
 
- document.write(score);
 
 }
